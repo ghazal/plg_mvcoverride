96f5ab02cefceb711e4a0941f3bec9626465726f
ghazal <ghazal@users.noreply.github.com>
2014-12-03 14:24:17 +0100

    update of leftovers from old code

--- mvcoverride.php
+++ mvcoverride.php
@@ -106,7 +106,9 @@ class PlgSystemMVCOverride extends JPlugin
 						
 						if (count($definesSourceOverride[0]))
 						{
-							throw new DatabaseException('Plugin MVC Override','Your override file use constants, please replace code constants<br />JPATH_COMPONENT -> JPATH_SOURCE_COMPONENT,<br />JPATH_COMPONENT_SITE -> JPATH_SOURCE_COMPONENT_SITE and<br />JPATH_COMPONENT_ADMINISTRATOR -> JPATH_SOURCE_COMPONENT_ADMINISTRATOR');
+						
+						//throw new Exception(JText::_('Ckjhkjhkjh'));
+							throw new Exception(JText::_('Plugin MVC Override','Your override file use constants, please replace code constants<br />JPATH_COMPONENT -> JPATH_SOURCE_COMPONENT,<br />JPATH_COMPONENT_SITE -> JPATH_SOURCE_COMPONENT_SITE and<br />JPATH_COMPONENT_ADMINISTRATOR -> JPATH_SOURCE_COMPONENT_ADMINISTRATOR')); 
 						}
 						else
 						{
