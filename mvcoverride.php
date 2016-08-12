cf80f9f0ee97d4a68776f278b85f59145c3920c7
ghazal <ghazal@users.noreply.github.com>
2016-08-12 12:52:47 +0200

    correction by vdrover

--- mvcoverride.php
+++ mvcoverride.php
@@ -49,7 +49,9 @@ class PlgSystemMVCOverride extends JPlugin
 			
 			$componentID = $menuDefault->componentid;
 			$db = JFactory::getDBO();
-			$db->setQuery('SELECT * FROM #__extensions WHERE id ='.$db->quote($componentID));
+			//$db->setQuery('SELECT * FROM #__extensions WHERE id ='.$db->quote($componentID));
+			//corrected by vdrover 
+			$db->setQuery('SELECT * FROM #__extensions WHERE extension_id ='.$db->quote($componentID));
 			$component = $db->loadObject();
 			$option = $component->element;
 		}
